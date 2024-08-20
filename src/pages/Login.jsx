705e473ef03d20567eca71df7781d59bef2e42f3
Mehedi Hasan Jim <mhjim.info@gmail.com>
2024-08-20 16:48:40 +0600

    Sign Up & Login Page Done!

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,12 @@ import signupImg from './../assets/Login.jpg'
 import { Link } from 'react-router-dom'
 import { Alert } from '@mui/material'
 import { RiEye2Line, RiEyeCloseLine } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const Signup = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
     let [email, setEmail] = useState('')
     let [password, setPassword] = useState('')
     let [emailerror, setEmailError] = useState('')
@@ -27,6 +31,42 @@ const Signup = () => {
         } if (!password) {
             setPasswordError(<Alert severity="error">This Field is Required</Alert>)
         }
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                if (error.code.includes('auth/invalid-credential')) {
+                    setPasswordError(<Alert severity="error">Email or Password is Wrong</Alert>);
+                }
+            });
+    }
+
+    let handleGoogleLogin = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+                console.log(error)
+            });
     }
 
     return (
@@ -34,8 +74,10 @@ const Signup = () => {
             <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-4">
                 <div>
                     <h1 className='text-2xl lg:text-4xl text-[#11175D] font-bold mb-3'>Login to your account!</h1>
-                    <p className='text-lg lg:text-xl text-[#00000050]'>Free register and you can enjoy it</p>
                     <div className='mt-8 lg:mt-[53px] flex flex-col gap-6 lg:gap-[48px] w-full max-w-md'>
+                        <div className="btnBox flex">
+                            <button className='flex gap-3 rounded-lg pl-[30px] pr-[42px] py-6 border border-[#03014C]/30 font-semibold text-sm text-[#03014C] hover:shadow-lg duration-500' onClick={handleGoogleLogin}><FcGoogle className='my-auto text-xl' />Login with Google</button>
+                        </div>
                         <div className='relative'>
                             <label className='absolute left-4 lg:left-[52px] top-[-12px] bg-white px-2 lg:px-[15px]'>Email Address</label>
                             <input onChange={handleEmail} type="email" className='w-full py-4 lg:py-[26px] pl-4 lg:pl-[52px] pr-4 lg:pr-[66px] border border-[#11175D50] rounded-lg text-lg lg:text-xl font-semibold text-[#11175D70]' />
@@ -55,12 +97,12 @@ const Signup = () => {
                                 <div className='py-1'>{passworderror}</div>
                             }
                         </div>
-                        <button onClick={handleSubmit} className='w-full py-4 lg:py-5 bg-black text-lg lg:text-xl text-white font-bold rounded-full lg:rounded-[86px] hover:bg-gradient-to-r from-[#11175D] to-black hover:rounded-lg duration-500 hover:shadow-md mb-8 lg:mb-[35px]'>Login to Continue</button>
+                        <button onClick={handleSubmit} className='w-full py-4 lg:py-5 bg-black text-lg lg:text-xl text-white font-bold rounded-full lg:rounded-[86px] hover:bg-gradient-to-r from-[#11175D] to-black hover:rounded-lg duration-500 hover:shadow-lg mb-8 lg:mb-[35px]'>Login to Continue</button>
                         <p className='text-center text-sm lg:text-base text-[#03014C]'>Don’t have an account ? <Link to="/signup" className='text-[#11175D] font-bold hover:text-[#11175D70] duration-500'>Sign up</Link></p>
                     </div>
                 </div>
             </div>
-            <div className="w-full lg:w-1/2">
+            <div className="w-full lg:w-1/2 hidden lg:block">
                 <img src={signupImg} alt="signup" className='w-full h-full object-cover' />
             </div>
         </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,65 +1,82 @@
-import React, { useState } from 'react'
-import signupImg from './../assets/Signup.png'
-import { Link, useNavigate } from 'react-router-dom'
-import { Alert } from '@mui/material'
+import React, { useState } from 'react';
+import signupImg from './../assets/Signup.png';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert } from '@mui/material';
 import { RiEye2Line, RiEyeCloseLine } from "react-icons/ri";
-import { Watch } from 'react-loader-spinner'
+import { Watch } from 'react-loader-spinner';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const Signup = () => {
-    let navigate = useNavigate()
-    let [email, setEmail] = useState('')
-    let [name, setName] = useState('')
-    let [password, setPassword] = useState('')
-    let [emailerror, setEmailError] = useState('')
-    let [nameerror, setNameError] = useState('')
-    let [passworderror, setPasswordError] = useState('')
+    let navigate = useNavigate();
+    let [email, setEmail] = useState('');
+    let [name, setName] = useState('');
+    let [password, setPassword] = useState('');
+    let [emailError, setEmailError] = useState('');
+    let [nameError, setNameError] = useState('');
+    let [passwordError, setPasswordError] = useState('');
 
-    let [passwordShow, setPasswordShow] = useState(false)
+    let [passwordShow, setPasswordShow] = useState(false);
 
-    let [success, setSuccess] = useState(false)
+    let [success, setSuccess] = useState(false);
 
     let handleEmail = (e) => {
-        setEmail(e.target.value)
-        setEmailError("")
+        setEmail(e.target.value);
+        setEmailError("");
     }
     let handleName = (e) => {
-        setName(e.target.value)
-        setNameError("")
+        setName(e.target.value);
+        setNameError("");
     }
     let handlePassword = (e) => {
-        setPassword(e.target.value)
-        setPasswordError("")
+        setPassword(e.target.value);
+        setPasswordError("");
     }
 
     let handleSubmit = () => {
+        let isValid = true;
+
         if (!email) {
-            setEmailError(<Alert severity="error">This Field is Required</Alert>)
-        } if (!password) {
-            setPasswordError(<Alert severity="error">This Field is Required</Alert>)
-        } if (!name) {
-            setNameError(<Alert severity="error">This Field is Required</Alert>)
+            setEmailError(<Alert severity="error">This Field is Required</Alert>);
+            isValid = false;
+        } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            setEmailError(<Alert severity="error">Invalid Email</Alert>);
+            isValid = false;
         }
+
+        if (!password) {
+            setPasswordError(<Alert severity="error">This Field is Required</Alert>);
+            isValid = false;
+        }
+
+        if (!name) {
+            setNameError(<Alert severity="error">This Field is Required</Alert>);
+            isValid = false;
+        }
+
+        if (!isValid) return;
 
         const auth = getAuth();
 
         if (email && name && password) {
-            setSuccess(true)
+            setSuccess(true);
             createUserWithEmailAndPassword(auth, email, password)
                 .then((userCredential) => {
+                    const user = userCredential.user;
                     setTimeout(() => {
-                        navigate('/')
-                        const user = userCredential.user;
+                        navigate('/');
                     }, 1000);
-
                 })
                 .catch((error) => {
+                    setSuccess(false);
                     const errorCode = error.code;
                     const errorMessage = error.message;
-                    // ..
+                    if (error.code.includes('auth/email-already-in-use')) {
+                        setEmailError(<Alert severity="error">Email Already in Use</Alert>);
+                    }
                 });
         }
-
     }
 
     return (
@@ -72,16 +89,12 @@ const Signup = () => {
                         <div className='relative'>
                             <label className='absolute left-4 lg:left-[52px] top-[-12px] bg-white px-2 lg:px-[15px]'>Email Address</label>
                             <input onChange={handleEmail} type="email" className='w-full py-4 lg:py-[26px] pl-4 lg:pl-[52px] pr-4 lg:pr-[66px] border border-[#11175D50] rounded-lg text-lg lg:text-xl font-semibold text-[#11175D70]' />
-                            {emailerror &&
-                                <div className='py-1'>{emailerror}</div>
-                            }
+                            {emailError && <div className='py-1'>{emailError}</div>}
                         </div>
                         <div className='relative'>
                             <label className='absolute left-4 lg:left-[52px] top-[-12px] bg-white px-2 lg:px-[15px]'>Full name</label>
                             <input onChange={handleName} type="text" className='w-full py-4 lg:py-[26px] pl-4 lg:pl-[52px] pr-4 lg:pr-[66px] border border-[#11175D50] rounded-lg text-lg lg:text-xl font-semibold text-[#11175D70]' />
-                            {nameerror &&
-                                <div className='py-1'>{nameerror}</div>
-                            }
+                            {nameError && <div className='py-1'>{nameError}</div>}
                         </div>
                         <div className='relative'>
                             <label className='absolute left-4 lg:left-[52px] top-[-12px] bg-white px-2 lg:px-[15px]'>Password</label>
@@ -91,11 +104,9 @@ const Signup = () => {
                                 :
                                 <RiEyeCloseLine onClick={() => setPasswordShow(true)} className='text-xl absolute top-1/2 transform -translate-y-1/2 right-5 cursor-pointer' />
                             }
-                            {passworderror &&
-                                <div className='py-1'>{passworderror}</div>
-                            }
+                            {passwordError && <div className='py-1'>{passwordError}</div>}
                         </div>
-                        <button onClick={handleSubmit} className='w-full py-4 lg:py-5 bg-black text-lg lg:text-xl text-white font-bold rounded-full lg:rounded-[86px] hover:bg-gradient-to-r from-[#11175D] to-black hover:rounded-lg duration-500 hover:shadow-md mb-8 lg:mb-[35px] flex justify-center items-center'>
+                        <button onClick={handleSubmit} className='w-full py-4 lg:py-5 bg-black text-lg lg:text-xl text-white font-bold rounded-full lg:rounded-[86px] hover:bg-gradient-to-r from-[#11175D] to-black hover:rounded-lg duration-500 hover:shadow-lg mb-8 lg:mb-[35px] flex justify-center items-center'>
                             {success ?
                                 <Watch
                                     visible={true}
@@ -109,17 +120,16 @@ const Signup = () => {
                                 />
                                 : "Sign Up"}
                         </button>
-
-
                         <p className='text-center text-sm lg:text-base text-[#03014C]'>Already have an account? <Link to="/" className='text-[#11175D] font-bold hover:text-[#11175D70] duration-500'>Sign In</Link></p>
                     </div>
                 </div>
             </div>
-            <div className="w-full lg:w-1/2">
+            <div className="w-full lg:w-1/2 hidden lg:block">
                 <img src={signupImg} alt="signup" className='w-full h-full object-cover' />
             </div>
+            <ToastContainer />
         </div>
     )
 }
 
-export default Signup
+export default Signup;
