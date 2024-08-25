bd9e297201aee256ce2764266f79d13383f0e586
Mehedi Hasan Jim <mhjim.info@gmail.com>
2024-08-25 14:52:32 +0600

    why this is so hard?

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { CiMenuKebab } from "react-icons/ci";
 
 const GroupList = () => {
     return (
-        <div>
-
+        <div className="w-[427px] h-[347px] rounded-[20px] shadow-lg pt-[13px] pb-[20px] pl-[20px] pr-[22px]">
+            <div className="flex justify-between mb-[17px]">
+                <h4 className="font-semibold text-xl text-textColor">Group List</h4>
+                <CiMenuKebab className="text-2xl my-auto" />
+            </div>
+            <div className="px-[5px]">
+                {/* users */}
+            </div>
         </div>
     )
 }
