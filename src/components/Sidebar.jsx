6fa4964c4c42a09bc9fac4355cc26c677809bdef
Mehedi Hasan Jim <mhjim.info@gmail.com>
2024-08-25 14:31:01 +0600

    Working on Dashboard Design

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import profile from '../assets/profile.png'
+import React from 'react';
+import profile from '../assets/profile.png';
 import { FaHome, FaBell } from "react-icons/fa";
 import { FaInbox, FaGear } from "react-icons/fa6";
 import { IoMdLogOut } from "react-icons/io";
-import { Link } from 'react-router-dom';
-
-
+import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
+    const location = useLocation();
+
+    const isDashboardActive = location.pathname === '/' || location.pathname === '/dashboard';
+
     return (
-        <section id='sidebar' className='w-[260px] h-screen bg-gray-50 pl-[32px] pr-[43px] py-[35px]'>
+        <section id='sidebar' className='w-[260px] h-screen pl-[32px] pr-[43px] py-[35px]'>
             <div className="w-full h-full bg-black rounded-[20px] pt-[38px] pb-[48px] flex flex-col justify-between">
                 <img src={profile} alt="profile pic" className='w-[100px] h-[100px] rounded-full mx-auto' />
                 <ul className=' flex flex-col items-center gap-[82px] text-[46px] text-gray-500 mt-[-78px]'>
-                    <li className=''><Link to='/dashboard'><FaHome /></Link></li>
-                    <li><Link to='/messages'><FaInbox /></Link></li>
-                    <li><Link to='/notifications'><FaBell /></Link></li>
-                    <li><Link to='/settings'><FaGear /></Link></li>
+                    <li className={`${isDashboardActive ? 'text-white' : ''}`}>
+                        <Link to='/'><FaHome /></Link>
+                    </li>
+                    <li className={`${location.pathname === '/messages' ? 'text-white' : ''}`}>
+                        <Link to='/messages'><FaInbox /></Link>
+                    </li>
+                    <li className={`${location.pathname === '/notifications' ? 'text-white' : ''}`}>
+                        <Link to='/notifications'><FaBell /></Link>
+                    </li>
+                    <li className={`${location.pathname === '/settings' ? 'text-white' : ''}`}>
+                        <Link to='/settings'><FaGear /></Link>
+                    </li>
                 </ul>
                 <IoMdLogOut className='text-[46px] text-white mx-auto' />
             </div>
         </section>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { RiSearch2Line } from "react-icons/ri";
+import { CiMenuKebab } from "react-icons/ci";
 
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard
+        <div className="w-full py-[35px]">
+            <div className="w-1/3">
+                <div className="relative">
+                    <input type="text" placeholder="Search" className="max-w-[427px] h-[59px] pl-[78px] pr-[24px] border border-textColor/20 shadow-lg outline-none rounded-[20px] placeholder:text-[#3D3D3D]/35 placeholder:text-[16px] placeholder:font-medium" />
+                    <RiSearch2Line className="text-xl absolute left-6 top-[50%] translate-y-[-50%]" />
+                    <CiMenuKebab className="text-xl absolute right-[22px] top-[50%] translate-y-[-50%]" />
+                </div>
+
+            </div>
         </div>
     )
 }
