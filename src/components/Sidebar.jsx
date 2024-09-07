280ef845217406030b86c0dbf4e62d58e35068f3
Mehedi Hasan Jim <mhjim.info@gmail.com>
2024-09-07 17:44:04 +0600

    profile pic update not finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaHome, FaBell } from "react-icons/fa";
 import { FaInbox, FaGear } from "react-icons/fa6";
-import { IoCameraReverseOutline } from "react-icons/io5";
+import { IoCameraReverseOutline, IoCloseCircle } from "react-icons/io5";
 import { IoMdLogOut } from "react-icons/io";
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 
 const Sidebar = () => {
+    let data = useSelector((state) => state.userInfo.value)
+
+    const storage = getStorage();
+
     const location = useLocation();
     const isDashboardActive = location.pathname === '/' || location.pathname === '/dashboard';
 
-    let data = useSelector((state) => state.userInfo.value)
+    let [imageModal, setImageModal] = useState(false)
+
+    let [imageFile, setImageFile] = useState(null)
+
+    let handleImageFile = (e) => {
+        setImageFile(e.target.files[0])
+    }
+
+    let handleUpload = () => {
+        const storageRef = ref(storage, 'profile-pic');
+        uploadBytes(storageRef, imageFile).then((snapshot) => {
+            getDownloadURL(storageRef).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+            });
+        });
+    }
 
 
     return (
@@ -20,7 +40,7 @@ const Sidebar = () => {
                 <div className="flex flex-col gap-4 mx-auto text-white items-center group cursor-pointer">
                     <div className="w-[100px] h-[100px] rounded-full bg-red-100 relative overflow-hidden">
                         <img src={data.photoURL} alt="profile pic" className='w-full h-full' />
-                        <div className="w-full h-full bg-black/40 absolute top-0 left-0 flex justify-center items-center text-2xl opacity-0 group-hover:opacity-100 duration-300">
+                        <div onClick={() => setImageModal(true)} className="w-full h-full bg-black/40 absolute top-0 left-0 flex justify-center items-center text-2xl opacity-0 group-hover:opacity-100 duration-300">
                             <IoCameraReverseOutline />
                         </div>
                     </div>
@@ -69,6 +89,29 @@ const Sidebar = () => {
                     </span>
                 </div>
             </div>
+            {imageModal &&
+                <div className="z-10 bg-black/30 absolute top-0 left-0 w-full h-full flex justify-center items-center">
+                    <div className="w-[650px] h-[420px] bg-white rounded-lg flex flex-col justify-between overflow-hidden">
+                        <div className="w-full h-[60px] bg-black flex items-center px-[30px] justify-between">
+                            <h4 className='text-white font-bold'>Upload Photo!</h4>
+                            <IoCloseCircle className='text-white font-normal text-xl cursor-pointer' onClick={() => setImageModal(false)} />
+                        </div>
+                        <div className="w-[75%] p-12 mx-auto flex justify-center items-center border-[4px] border-dotted">
+                            <label className="cursor-pointer flex flex-col items-center">
+                                <input type="file" className=" text-textColor text-sm px-4 py-2 rounded-md " onChange={handleImageFile} />
+                                {/* <div className="bg-textColor text-white px-4 py-2 rounded-md shadow-md hover:bg-black transition-colors duration-300">
+                                    Upload File
+                                </div> */}
+                            </label>
+                        </div>
+                        <div className="w-full h-[60px] border flex items-center justify-end gap-4 px-[30px]">
+                            <button onClick={() => setImageModal(false)} className='bg-gray-200 py-1 px-3 border border-slate-300 rounded-full'>Cancel</button>
+                            <button onClick={handleUpload} className='bg-black text-white py-1 px-3 border border-slate-300 rounded-full'>Upload</button>
+                        </div>
+                    </div>
+                </div>
+
+            }
         </section>
     );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -55,28 +55,28 @@ const Dashboard = () => {
             {verify ?
 
                 <div className="flex flex-wrap gap-7 py-[35px]">
-                    <div className="w-[365px] h-[451px]">
+                    <div className="w-[365px] h-[425px]">
                         <Searchbar />
                     </div>
-                    <div className="w-[365px] h-[451px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
+                    <div className="w-[365px] h-[425px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
                         <GroupList />
                     </div>
-                    <div className="w-[365px] h-[451px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
+                    <div className="w-[365px] h-[425px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
                         <UserList />
                     </div>
-                    <div className="w-[365px] h-[451px]">
+                    <div className="w-[365px] h-[425px]">
                         <a href="https://10ms.io/ug9dhN"><img src={Ad} alt="" className="w-full h-full" /></a>
                     </div>
-                    <div className="w-[365px] h-[451px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
+                    <div className="w-[365px] h-[425px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
                         <Friends />
                     </div>
-                    <div className="w-[365px] h-[451px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
+                    <div className="w-[365px] h-[425px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
                         <FriendRequest />
                     </div>
-                    <div className="w-[365px] h-[451px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
+                    <div className="w-[365px] h-[425px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
                         <MyGroups />
                     </div>
-                    <div className="w-[365px] h-[451px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
+                    <div className="w-[365px] h-[425px] bg-white shadow-md rounded-[20px] p-5 text-center border border-gray-300">
                         <Blocked />
                     </div>
                 </div>
