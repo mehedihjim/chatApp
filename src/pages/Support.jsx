7b063c8fa6d13f6155d4103e9d637edb0734d2e6
mehedihjim <mhjim.info@gmail.com>
2024-09-19 22:57:54 +0600

    Support page

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,5 +1,6 @@
 import mhjim from '../assets/mhjim.png'
 import { FaScrewdriverWrench } from "react-icons/fa6";
+import { IoHelpCircleOutline } from "react-icons/io5";
 
 const Support = () => {
     return (
@@ -7,9 +8,10 @@ const Support = () => {
             <div className="mx-auto w-[20%] opacity-80 mb-6">
                 <img src={mhjim} alt="" />
             </div>
-            <div className="text-3xl font-semibold text-textColor">
-                <h2 className='flex gap-3 justify-center'><FaScrewdriverWrench className='my-auto' /> Support & Help Center</h2>
-                <p></p>
+            <div className="">
+                <h2 className='text-3xl font-semibold text-textColor flex gap-3 justify-center mb-8'><FaScrewdriverWrench className='my-auto' /> Support & Help Center</h2>
+                <p className='flex w-full border border-slate-600 shadow-lg bg-slate-200 rounded-lg px-6 py-10 gap-1 '><IoHelpCircleOutline className=' text-textColor text-2xl' />
+                    An open-source chat application designed for seamless real-time communication. Easily create chat rooms, connect with others, and enjoy a user-friendly interface. Built with simplicity and collaboration in mind, this app is perfect for developers and users looking for a lightweight, customizable chat solution.</p>
             </div>
         </div>
     )
