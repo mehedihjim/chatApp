af2cde95fab36fa25aa733e180f8ca2f164c96ba
mehedihjim <mhjim.info@gmail.com>
2024-09-19 22:35:46 +0600

    Support Page Design

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import mhjim from '../assets/mhjim.png'
+import { FaScrewdriverWrench } from "react-icons/fa6";
 
 const Support = () => {
     return (
-        <div>
-
+        <div className="w-full overflow-hidden py-9 pr-[46px] flex flex-col">
+            <div className="mx-auto w-[20%] opacity-80 mb-6">
+                <img src={mhjim} alt="" />
+            </div>
+            <div className="text-3xl font-semibold text-textColor">
+                <h2 className='flex gap-3 justify-center'><FaScrewdriverWrench className='my-auto' /> Support & Help Center</h2>
+                <p></p>
+            </div>
         </div>
     )
 }
